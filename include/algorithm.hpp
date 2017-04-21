05688da38f53c5681b26cfaab5136f891320ed9f
Tommy Nguyen <remyabel@gmail.com>
2017-04-20 21:32:26 -0400

    Fix unsigned vs signed comparison

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -1,11 +1,14 @@
 #ifndef ALGORITHM_HPP
 #define ALGORITHM_HPP
 
+#include <stddef.h>
+
 // [alg.copy]
 template <class InputIterator, class OutputIterator>
 OutputIterator copy(InputIterator first, InputIterator last, OutputIterator result)
 {
-    for (auto n = 0u; n < (last - first); ++n) {
+    size_t dist = last - first;
+    for (auto n = 0u; n < dist; ++n) {
         *(result + n) = *(first + n);
     }
     return result + (last - first);
