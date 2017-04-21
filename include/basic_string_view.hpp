b8cf3d73e6b6221b867bcd24c864fcce74bcf3d2
Tommy Nguyen <remyabel@gmail.com>
2017-04-20 23:06:16 -0400

    [#1] string.view.iterators

--- include/basic_string_view.hpp
+++ include/basic_string_view.hpp
@@ -13,7 +13,12 @@ public:
     using pointer         = value_type*;
     using const_pointer   = const value_type*;
     using const_reference = const value_type&;
+    
+    using const_iterator  = const_pointer;
+    using iterator        = const_iterator;
+
     using size_type       = size_t;
+    using difference_type = ptrdiff_t;
 
     static constexpr size_type npos = size_type(-1);
 
@@ -29,7 +34,29 @@ public:
     constexpr basic_string_view(const charT *str, size_type len)
         : size_(len), data_(str) { }
 
+    // [string.view.iterators]
+    
+    constexpr const_iterator begin() const noexcept {
+        return data_;
+    }
+
+    constexpr const_iterator cbegin() const noexcept {
+        return begin();
+    }
+
+    constexpr const_iterator end() const noexcept {
+        return begin() + size();
+    }
+
+    constexpr const_iterator cend() const noexcept {
+        return end();
+    }
+
     // [string.view.capacity]
+    constexpr size_type size() const noexcept {
+        return size_;
+    }
+
     constexpr size_type length() const noexcept {
         return size_;
     }
--- test/test-basicstringview.cpp
+++ test/test-basicstringview.cpp
@@ -1,5 +1,6 @@
 #include "catch.hpp"
 
+#include <iterator.hpp>
 #include <basic_string_view.hpp>
 
 TEST_CASE("basic_string_view constructors", "[basic_string_view]") {
@@ -23,4 +24,26 @@ TEST_CASE("basic_string_view constructors", "[basic_string_view]") {
         REQUIRE(char_traits<char>::compare("long", s2.data(), 4) == 0);
     }
 
+}
+
+TEST_CASE("basic_string_view interface", "[basic_string_view.interface]") {
+    string_view s{"hello"};
+    string_view s2{"long string", 4};
+
+    SECTION("iterators") {
+        REQUIRE(s.begin() == s.cbegin());
+        REQUIRE(*s.begin() == 'h');
+        REQUIRE(next(s.begin(), 5) == s.end());
+        REQUIRE(s.size() == s.length());
+
+        REQUIRE(s2.size() == 4);
+        REQUIRE(s2.size() == (s.size() - 1));
+
+        REQUIRE(s.begin() != s2.begin());
+    }
+
+    SECTION("access") {
+        REQUIRE(s[0] == 'h');
+        REQUIRE(s2[0] == 'l');
+    }
 }
