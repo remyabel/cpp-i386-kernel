1cce5f069df2ae688aadf799236bb63744e9fac7
Tommy Nguyen <remyabel@gmail.com>
2017-04-23 18:03:16 -0400

    Remove static keyword from namespace functions

--- include/Terminal.hpp
+++ include/Terminal.hpp
@@ -46,14 +46,14 @@ namespace vga {
         }
     };
 
-    static color make_color(color fg, color bg) {
+    color make_color(color fg, color bg) {
         return static_cast<color>(
                 static_cast<uint8_t>(fg) 
                 | (static_cast<uint8_t>(bg) << 4)
             );
     }
     
-    static colored_char make_colored_char(char code_byte, color attribute) {
+    colored_char make_colored_char(char code_byte, color attribute) {
         return { code_byte, attribute };
     }
     
