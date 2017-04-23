819d6251b6354596f1bd93b72af94fdd664022c0
Tommy Nguyen <remyabel@gmail.com>
2017-04-23 14:31:17 -0400

    Factor implementation into cpp; define global in one TU

--- include/Terminal.hpp
+++ include/Terminal.hpp
@@ -51,13 +51,7 @@ class Terminal {
     color color_;
     uint16_t *buffer_;
 public:
-    Terminal() 
-        : row_(0), column_(0),
-          color_(make_color(color::white, color::black)),
-          buffer_(reinterpret_cast<uint16_t*>(0xB8000))
-    { 
-        clear();
-    }
+    Terminal();
 
     static constexpr size_t columns = 80;
     static constexpr size_t rows = 25;
@@ -73,80 +67,21 @@ public:
         return { code_byte, attribute };
     }
 
-    void set_color(color c) {
-        color_ = c;
-    }
+    void set_color(color c);
 
-    void write_char_at(char c, color color, size_t x, size_t y) {
-        const size_t index = y * columns + x;
-        buffer_[index] = make_colored_char(c, color);
-    }
+    void write_char_at(char c, color color, size_t x, size_t y);
 
-    void put_char(char c) {
-        if (c == '\n') {
-            column_ = 0;
-            row_++;
-        } else {
-            write_char_at(c, color_, column_, row_);
-            column_++;
-        }
+    void put_char(char c);
+                
+    void write(string_view data);
 
-        if (column_ == columns) {
-            column_ = 0;
-            ++row_;
-        }
+    void clear();
 
-        if (row_ == rows) {
-            row_ = rows - 1;
+    void clear_line(size_t row);
 
-            // Copy each line upwards
-            for (auto y = 0u; y < rows; y++) {
-                auto next_line_start    = (y + 1) * columns;
-                auto next_line_end      = ((y + 1) * columns) + columns;
-                auto current_line_start = y * columns;
-
-                copy(buffer_ + next_line_start,
-                        buffer_ + next_line_end,
-                        buffer_ + current_line_start);
-            }
-
-            clear_line(rows - 1);
-
-            
-        }
-
-        move_cursor();
-    }
-    
-    void write(string_view data) {
-        for (size_t i = 0; i < data.length(); i++) {
-            put_char(data[i]);
-        }
-    }
-
-    void clear() {
-        for (auto y = 0u; y < rows; ++y) {
-            for (auto x = 0u; x < columns; ++x) {
-                write_char_at(' ', make_color(color::white, color::black), x, y);
-            }
-        }
-    }
-
-    void clear_line(size_t row) {
-        for (auto x = 0u; x < columns; ++x) {
-            write_char_at(' ', make_color(color::white, color::black), x, row);
-        }
-    }
-
-    void move_cursor() {
-        auto position = (row_ * columns) + column_;
-
-        outb(vga_index_register::underline_location, vga_index_register::high_byte);
-        outb(vga_index_register::underline_location + 1, static_cast<uint8_t>(position >> 8));
-
-        outb(vga_index_register::underline_location, vga_index_register::low_byte);
-        outb(vga_index_register::underline_location + 1, static_cast<uint8_t>(position));
-    }
+    void move_cursor();
 };
+
+extern Terminal terminal;
 
 #endif
