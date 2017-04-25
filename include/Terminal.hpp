3e6dab88f01bcaab021149738129d11f5075ea23
Tommy Nguyen <remyabel@gmail.com>
2017-04-25 01:35:51 -0400

    Revert 1cce5f0 'Remove static keyword...'

--- include/Terminal.hpp
+++ include/Terminal.hpp
@@ -46,14 +46,14 @@ namespace vga {
         }
     };
 
-    color make_color(color fg, color bg) {
+    static color make_color(color fg, color bg) {
         return static_cast<color>(
                 static_cast<uint8_t>(fg) 
                 | (static_cast<uint8_t>(bg) << 4)
             );
     }
     
-    colored_char make_colored_char(char code_byte, color attribute) {
+    static colored_char make_colored_char(char code_byte, color attribute) {
         return { code_byte, attribute };
     }
     
