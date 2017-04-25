f9a340b6aa6a2fa6202fe313a09fcbc1ba0fa6fd
Tommy Nguyen <remyabel@gmail.com>
2017-04-25 05:05:45 -0400

    Use inline namespace instead of static

--- include/Terminal.hpp
+++ include/Terminal.hpp
@@ -48,15 +48,17 @@ namespace vga {
         }
     };
 
-    static color make_color(color fg, color bg) {
-        return static_cast<color>(
-                static_cast<uint8_t>(fg) 
-                | (static_cast<uint8_t>(bg) << 4)
-            );
-    }
-    
-    static colored_char make_colored_char(char code_byte, color attribute) {
-        return { code_byte, attribute };
+    inline namespace {
+        color make_color(color fg, color bg) {
+            return static_cast<color>(
+                    static_cast<uint8_t>(fg) 
+                    | (static_cast<uint8_t>(bg) << 4)
+                );
+        }
+        
+        colored_char make_colored_char(char code_byte, color attribute) {
+            return { code_byte, attribute };
+        }
     }
     
     class Terminal {
