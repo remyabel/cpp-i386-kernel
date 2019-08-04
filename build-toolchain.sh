97cad3afac5a92dcd4b30c4f278dc18864d01fc3
Tommy Nguyen <remyabel@gmail.com>
2019-08-04 15:47:35 -0400

    Update GCC and cleanup build script.
    - Put the versions in variables.
    
    - We no longer need to check if cross-install is empty since we
    are not using Travis anymore.

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-if [ -n "$(find "$HOME"/cross-install -maxdepth 0 -empty)" ]
+readonly GCC_VERSION=9.1.0
+readonly BINUTILS_VERSION=2.31
+
+mkdir -p /tmp/toolchain && cd /tmp/toolchain || exit
+
+if [ ! -f "binutils-$BINUTILS_VERSION.tar.gz" ]
 then
-    mkdir -p /tmp/toolchain && cd /tmp/toolchain || exit
-
-    if [ ! -f "binutils-2.31.tar.gz" ]
-    then
-        wget https://ftp.gnu.org/gnu/binutils/binutils-2.31.tar.gz
-        tar zxf binutils-2.31.tar.gz
-    fi
-
-    if [ ! -f "gcc-8.2.0.tar.gz" ]
-    then
-        wget https://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.gz
-        tar zxf gcc-8.2.0.tar.gz
-    fi
-
-    mkdir -p /tmp/toolchain/build-binutils
-    mkdir -p /tmp/toolchain/build-gcc
-
-    cd /tmp/toolchain/build-binutils && rm -rf ./*
-    /tmp/toolchain/binutils-2.31/configure --target=i686-elf --prefix="$HOME/cross-install" --with-sysroot --disable-nls --disable-werror
-    make -j4 && make install
-
-    cd /tmp/toolchain/gcc-8.2.0 && ./contrib/download_prerequisites
-    cd /tmp/toolchain/build-gcc && rm -rf ./*
-    /tmp/toolchain/gcc-8.2.0/configure --target=i686-elf --prefix="$HOME/cross-install" --disable-nls --enable-languages=c,c++ --without-headers
-    make -j4 all-gcc
-    make -j4 all-target-libgcc
-    make install-gcc
-    make install-target-libgcc
+    wget "https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz"
+    tar zxf "binutils-$BINUTILS_VERSION.tar.gz"
 fi
+
+if [ ! -f "gcc-$GCC_VERSION.tar.gz" ]
+then
+    wget "https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz"
+    tar zxf "gcc-$GCC_VERSION.tar.gz"
+fi
+
+mkdir -p /tmp/toolchain/build-binutils
+mkdir -p /tmp/toolchain/build-gcc
+
+cd /tmp/toolchain/build-binutils && rm -rf ./*
+"/tmp/toolchain/binutils-$BINUTILS_VERSION/configure" --target=i686-elf --prefix="$HOME/cross-install" --with-sysroot --disable-nls --disable-werror
+make -j4 && make install
+
+cd "/tmp/toolchain/gcc-$GCC_VERSION" && ./contrib/download_prerequisites
+cd /tmp/toolchain/build-gcc && rm -rf ./*
+"/tmp/toolchain/gcc-$GCC_VERSION/configure" --target=i686-elf --prefix="$HOME/cross-install" --disable-nls --enable-languages=c,c++ --without-headers
+make -j4 all-gcc
+make -j4 all-target-libgcc
+make install-gcc
+make install-target-libgcc
